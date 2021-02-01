cce173da728405e90092d58d1e68c512fa50afa5
Samuel Trahan <samtrahan91@gmail.com>
2021-02-01 13:33:48 -0600

    nav bar complete so far

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,15 @@ import './NavBar.css';
 export default function NavBar() {
     return (
         <div className="footer-nav-bar">
-          
+            <div className="start">
+                <h3>Start at the bottom</h3>
+            </div>
+          <div className="logo">
+              <img className="bottom-img" src="images/bottomUp.png" alt=""></img>
+          </div>
+          <div className="social-link">
+              <a className="samuel" href="https://www.samueltrahan.com" target="_blank" rel="noreferrer">Find Me</a>
+          </div>
         </div>
     )
 }
